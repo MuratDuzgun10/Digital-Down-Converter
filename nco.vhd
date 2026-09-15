ea52e8d60aeed03ca24f1a8437dde0569193d854
MuratDuzgun10 <kml.duzgun@gmail.com>
2026-09-15 22:29:23 +0300

    Update nco.vhd

--- nco.vhd
+++ nco.vhd
@@ -1,22 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 06.07.2026 22:16:49
--- Design Name: 
--- Module Name: nco - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
